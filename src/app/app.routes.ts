d309c1f0be0ae3c7703592850c41e521dd2adedb
mohit <mapinuly88@gmail.com>
2024-06-28 11:39:38 +0530

    add the dashboard page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PartnersComponent } from './pages/partners/partners.component';
+import { DataEntryComponent } from './pages/data-entry/data-entry.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+  },
+  {
+    path: 'partner',
+    component: PartnersComponent,
+  },
+  {
+    path: 'data-entry',
+    component: DataEntryComponent,
+  },
+];
